dfa9c028d06dd4a87912812c6af86c642ea8faed
Greg Taylor <gtaylor@duointeractive.com>
2009-12-16 19:02:08 +0000

    Upping VERSION in preparation for future release.

--- fedex/__init__.py
+++ fedex/__init__.py
@@ -52,4 +52,4 @@ If you have any questions, problems, ideas, or patch submissions, please visit
 our U{Google Code project<http://code.google.com/p/python-fedex/>} and enter
 an issue in the U{Issue Tracker<http://code.google.com/p/python-fedex/issues/list>}.
 """
-VERSION = '1.0.1'
+VERSION = '1.0.3'
