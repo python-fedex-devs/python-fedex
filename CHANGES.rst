38fb0d40c07449fdb4b04aaf8d048257e28ad8f1
Greg Taylor <gtaylor@l11solutions.com>
2012-10-21 15:59:24 -0400

    Packaging tweak.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,45 +1,42 @@
-==========
 Change Log
 ==========
 
------------------
+1.0.14
+------
+
+* Re-licensed under BSD.
+
 1.0.12 and 1.0.13
 -----------------
 
 * Forget that these ever existed.
 
-------
 1.0.11
 ------
 
 * Bug fix of a bug fix for regions config. (jcartmell)
 
-------
 1.0.10
 ------
 
 * Bug fix regarding regions and when they are sent. (jcartmell)
 
------
 1.0.9
 -----
 
 * Various fixes to RateRequest() FedEx API call. (jcartmell)
 * Added this changelog. (gtaylor)
 
------
 1.0.8
 -----
 
 * Fixed some problems with the unit tests. (gtaylor)
 
------
 1.0.7
 -----
 
 * Fixed a bug with international rate request example. (gtaylor)
 
------
 1.0.6
 -----
 
@@ -47,14 +44,12 @@ Change Log
 * FedEx RateRequest call implemented. (yahtib)
 * FedEx Postal Inquiry call implemented. (yahtib)
 
------
 1.0.4
 -----
 
 * Removal of unecessary files. (gtaylor)
 * Documentation improvements. (gtaylor)
 
------
 1.0.3
 -----
 
--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python
-"""
- python-fedex 
- Copyright (C) 2009 Gregory Taylor
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from distutils.core import setup
 import fedex
 
@@ -45,7 +28,7 @@ setup(name='fedex',
       package_dir={'fedex': 'fedex'},
       package_data={'fedex': ['wsdl/*.wsdl', 'wsdl/test_server_wsdl/*.wsdl']},
       platforms=['Platform Independent'],
-      license='GPLv3',
+      license='BSD',
       classifiers=CLASSIFIERS,
       keywords=KEYWORDS,
       requires=['suds'],
