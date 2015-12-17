7443db62b6c4d47e208a903ca61b7c1b4c9a3e59
Rad <radzhome@gmail.com>
2015-12-17 10:59:22 -0500

    fix distutils requires

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(name='fedex',
       license='BSD',
       classifiers=CLASSIFIERS,
       keywords=KEYWORDS,
-      requires=['suds-jurko'],
+      requires=['suds'],
       install_requires=['suds-jurko'],
 )
