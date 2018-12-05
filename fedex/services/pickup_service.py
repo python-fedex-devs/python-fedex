4dbdee517b594a215e8f944c31dfd9c519883462
Daniel S <daniel@languagesinmotion.com>
2018-12-05 15:57:48 -0700

    Add support for PickupAvailabilityRequest

--- fedex/services/pickup_service.py
+++ fedex/services/pickup_service.py
@@ -65,3 +65,81 @@ class FedexCreatePickupRequest(FedexBaseService):
             CountryRelationship=self.CountryRelationship
         )
 
+
+class FedexPickupAvailabilityRequest(FedexBaseService):
+
+    def __init__(self, config_obj, *args, **kwargs):
+        self._config_obj = config_obj
+        # Holds version info for the VersionId SOAP object.
+        self._version_info = {
+            'service_id': 'disp',
+            'major': '17',
+            'intermediate': '0',
+            'minor': '0'
+        }
+        self.PickupType = None
+        self.AccountNumber = None
+        self.PickupAddress = None
+        self.PickupRequestType = None
+        self.DispatchDate = None
+        self.NumberOfBusinessDays = None
+        self.PackageReadyTime = None
+        self.CustomerCloseTime = None
+        self.Carriers = None
+        self.ShipmentAttributes = None
+        self.PackageDetails = None
+        super(FedexPickupAvailabilityRequest, self).__init__(self._config_obj, 'PickupService_v17.wsdl', *args, **kwargs)
+
+    def _prepare_wsdl_objects(self):
+        self.Carriers = 'FDXE'
+
+        self.AccountNumber = self.client.factory.create('AssociatedAccount')
+        self.AccountNumber.Type = None
+        self.AccountNumber.AccountNumber = None
+
+        self.PickupAddress = self.client.factory.create('Address')
+
+        self.ShipmentAttributes = self.client.factory.create('PickupShipmentAttributes')
+        self.ShipmentAttributes.ServiceType = None
+        self.ShipmentAttributes.PackagingType = None
+
+        self.ShipmentAttributes.Dimensions = self.client.factory.create('Dimensions')
+        self.ShipmentAttributes.Dimensions.Length = None
+        self.ShipmentAttributes.Dimensions.Width = None
+        self.ShipmentAttributes.Dimensions.Height = None
+        self.ShipmentAttributes.Dimensions.Units = None
+
+        self.ShipmentAttributes.Weight = self.client.factory.create('Weight')
+        self.ShipmentAttributes.Weight.Units = None
+        self.ShipmentAttributes.Weight.Value = None
+
+        self.PackageDetails = self.client.factory.create('RequestedPickupPackageDetail')
+        self.PackageDetails.PackageSpecialServices = self.client.factory.create('PickupPackageSpecialServicesRequested')
+        self.PackageDetails.Weight = self.client.factory.create('Weight')
+
+    def _assemble_and_send_request(self):
+        """
+        Fires off the Fedex request.
+
+        @warning: NEVER CALL THIS METHOD DIRECTLY. CALL send_request(),
+            WHICH RESIDES ON FedexBaseService AND IS INHERITED.
+        """
+
+        # Fire off the query.
+        return self.client.service.getPickupAvailability(
+            WebAuthenticationDetail=self.WebAuthenticationDetail,
+            ClientDetail=self.ClientDetail,
+            TransactionDetail=self.TransactionDetail,
+            Version=self.VersionId,
+            PickupType=self.PickupType,
+            AccountNumber=self.AccountNumber,
+            PickupAddress=self.PickupAddress,
+            PickupRequestType=self.PickupRequestType,
+            DispatchDate=self.DispatchDate,
+            NumberOfBusinessDays=self.NumberOfBusinessDays,
+            PackageReadyTime=self.PackageReadyTime,
+            CustomerCloseTime=self.CustomerCloseTime,
+            Carriers=self.Carriers,
+            ShipmentAttributes=self.ShipmentAttributes,
+            PackageDetails=self.PackageDetails
+        )
